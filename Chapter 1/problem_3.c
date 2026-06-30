d8dfd46fc7eade214115dcb46c42b3b033263711
Spandan <spandanmondol08@gmail.com>
2026-06-30 16:45:25 +0530

    Add: Problem_3

---
+++ Chapter 1/problem_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int r,h;
+  printf("Enter the radius of the cylinder: ");
+  scanf("%d", &r);
+  printf("Enter the height of the cylinder: ");
+  scanf("%d", &h);
+  float volume = 3.14*r*r*h;
+  printf("Volume of the cylinder is : %f", volume);
+
+  return 0;
+}
