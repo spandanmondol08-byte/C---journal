e053c8c8eba431050ffb782d257d595327bb16db
Spandan <spandanmondol08@gmail.com>
2026-07-02 05:55:04 +0530

    Add : Project_1

--- Project 1 - Number guessing game/project_1.c
+++ Project 1 - Number guessing game/project_1.c
@@ -10,21 +10,21 @@ int main()
   {
     char ch;
     printf("Press Y to play and N to EXIT!! game : ");
-    scanf(" %c", &ch);  
-    if (ch=='Y')
+    scanf(" %c", &ch);
+    if (ch == 'Y')
     {
-      srand(time(NULL));   
+      srand(time(NULL));
 
       int num = rand() % 100 + 1;
-      int c=0;
-      while(1)
+      int c = 0;
+      while (1)
       {
         int guess;
-        while(1)
+        while (1)
         {
           printf("Enter a guess number between 1 to 100 : ");
-          scanf("%d",&guess);
-          if (guess > 100 || guess < 1 )
+          scanf("%d", &guess);
+          if (guess > 100 || guess < 1)
           {
             printf("**** Enter a Valid guess between 1 to 100 ****\n");
             continue;
@@ -34,28 +34,26 @@ int main()
             break;
           }
         }
-        
-        if (guess>num)
+
+        if (guess > num)
         {
           printf("LOWER!!!\n");
-          c=c+1;
+          c = c + 1;
         }
-        else if (guess<num)
+        else if (guess < num)
         {
           printf("HIGHER!!!\n");
-          c=c+1;
+          c = c + 1;
         }
         else
         {
-          c=c+1;
+          c = c + 1;
           printf("YOU WON!!!!\n");
           printf("You guessed the number\n");
-          printf("You took %d chances to guess correctly....\n",c);
+          printf("You took %d chances to guess correctly....\n", c);
           break;
         }
-    
       }
-
     }
     else
     {
