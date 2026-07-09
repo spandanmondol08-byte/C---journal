55e20e01e6abfe0de4e3abd00ca41652d8dd8562
Spandan <spandanmondol08@gmail.com>
2026-07-10 01:16:20 +0530

    ADD: Project_4_Date

--- Project 4 -Digital Clock/Project_4.c
+++ Project 4 -Digital Clock/Project_4.c
@@ -1,40 +1,71 @@
 #include <stdio.h>
 #include <time.h>
 
+
 struct tm *current_time;
 void gettime_1()
 {
   time_t raw_time;
   time(&raw_time);
   current_time = localtime(&raw_time);
-  printf("Current time : %02d:%02d:%02d ", current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
+  printf("Current time : %02d:%02d:%02d", current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
 }
 void gettime_2()
 {
+  int hour;
   time_t raw_time;
   time(&raw_time);
   current_time = localtime(&raw_time);
-  if (current_time->tm_hour / 12 == 0)
+  if (current_time->tm_hour == 0)
   {
-    current_time->tm_hour = 12;
+    hour = 12;
   }
-  else if (current_time->tm_hour / 12 <= 1)
+  else if (current_time->tm_hour / 12 > 0 && current_time->tm_hour / 12 <= 1)
   {
-    current_time->tm_hour = current_time->tm_hour;
+    hour = current_time->tm_hour;
   }
   else
   {
-    current_time->tm_hour = current_time->tm_hour % 12;
+    hour = current_time->tm_hour % 12;
   }
 
-  printf("Current time : %02d:%02d:%02d ", current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
+  printf("Current time : %02d:%02d:%02d", hour, current_time->tm_min, current_time->tm_sec);
+}
+
+void ampm()
+{
+  if (current_time->tm_hour>=12)
+  {
+    printf(" PM\n");
+  }
+  else
+  {
+    printf(" AM\n");
+  }
+}
+void getdate()
+{
+  char mon[10];
+  char wd[10];
+  char *months[] = {
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+  
+  int year=current_time->tm_year+1900;
+
+  char *weeks[] = {
+    "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+  
+    printf("Current date : %s %02d %s, %d",weeks[current_time->tm_wday],current_time->tm_mday,months[current_time->tm_mon],year);
+
 }
 
 int main()
 {
-  int ch;
+  /*int ch;
   printf("Enter 1 for 12 hrs / Enter 2 for 24 hrs format : ");
-  scanf("%d", &ch);
-  
+  scanf("%d", &ch);*/
+  gettime_2();
+  ampm();
+  getdate();
   return 0;
 }
