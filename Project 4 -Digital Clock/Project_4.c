90e52fbd9906266fc4da433f9013fbeabb88f7fe
Spandan <spandanmondol08@gmail.com>
2026-07-09 23:29:47 +0530

    ADD: Project_4

--- Project 4 -Digital Clock/Project_4.c
+++ Project 4 -Digital Clock/Project_4.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <time.h>
 
-int main() {
+void fill_time(char *buffer)
+{
   time_t raw_time;
   struct tm * current_time;
   time(& raw_time);
   current_time=localtime(&raw_time);
+  strftime(buffer,50,"%I:%M:%S %p",current_time);
+}
+int main() {
+  char time [50];
+  fill_time(time);
+  printf("Current time : %s",time);
 
   return 0;
 }
