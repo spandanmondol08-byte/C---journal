1704e0946d368de8bf7efa258b064387708064f2
Spandan <spandanmondol08@gmail.com>
2026-07-11 23:19:20 +0530

    Add : Project 7 v6

--- Project 7 -Bank Management System/project_7.c
+++ Project 7 -Bank Management System/project_7.c
@@ -16,41 +16,40 @@ typedef struct user_accounts
   int trans;
 } uac;
 
-void encrypter(uac users[30],int n)
+void encrypter(uac users[30], int n)
 {
   int shift = (users[n].account_number % 9) + 3;
-  for(int i = 0 ; users[n].password[i] != '\0';i++)
+  for (int i = 0; users[n].password[i] != '\0'; i++)
   {
     users[n].password[i] = users[n].password[i] + shift;
   }
 }
 
-void decrypter(uac users[30],int n)
+void decrypter(uac users[30], int n)
 {
   int shift = (users[n].account_number % 9) + 3;
-  for(int i = 0 ; users[n].password[i] != '\0';i++)
+  for (int i = 0; users[n].password[i] != '\0'; i++)
   {
     users[n].password[i] = users[n].password[i] - shift;
   }
 }
 
-
 void inputPassword(char password[])
 {
   int i = 0;
   char ch;
-  while ((ch = getch()) != '\r')   // Enter key
+  while ((ch = getch()) != '\r') // Enter key
   {
-    if (ch == '\b' && i > 0)     // Backspace
-      {
-        i--;
-        printf("\b \b");
-      }
+    if (ch == '\b' && i > 0) // Backspace
+    {
+      i--;
+      printf("\b \b");
+    }
     else if (ch != '\b' && i < 29)
-      {
-        password[i++] = ch;
-        printf("*");
-      }
+    {
+      password[i++] = ch;
+      printf("*");
+    }
   }
   password[i] = '\0';
   printf("\n");
@@ -63,19 +62,19 @@ void create_account(uac users[30], int n)
     int a = 676900000;
     int rn = rand() % 90000 + 10000;
     users[n].account_number = a + rn;
-    int ok=0;
+    int ok = 0;
     for (int i = 0; i < n; i++)
     {
       if (users[i].account_number == users[n].account_number)
       {
-        ok=ok+0;
+        ok = ok + 0;
       }
       else
       {
         ok++;
       }
     }
-    if (ok==n)
+    if (ok == n)
     {
       break;
     }
@@ -84,7 +83,7 @@ void create_account(uac users[30], int n)
   users[n].trans = 0;
   FILE *ptr;
   ptr = fopen("Bank_Info.txt", "a");
-  users[n].id=n+1;
+  users[n].id = n + 1;
   fprintf(ptr, "%d\n", users[n].id);
   printf("Enter the first name of account holder : ");
   getchar();
@@ -97,92 +96,110 @@ void create_account(uac users[30], int n)
   fprintf(ptr, "Last Name: %s\n", users[n].account_name_l);
   printf("Enter your password : ");
   inputPassword(users[n].password);
-  encrypter(users,n);
+  encrypter(users, n);
   fprintf(ptr, "Password: %s\n", users[n].password);
   fprintf(ptr, "Account Number: %d\n", users[n].account_number);
   fprintf(ptr, "Account Balance: %d\n", users[n].account_balance);
   fprintf(ptr, "Recent Transaction: %d\n", users[n].trans);
-  fprintf(ptr,"---------------------------------\n");
+  fprintf(ptr, "---------------------------------\n");
   fprintf(ptr, "\n");
   fclose(ptr);
   printf("\nAccount created successfully!\n\n");
   printf("---------------------------------\n");
-  printf("Account Holder Name : %s %s\n", users[n].account_name_f,users[n].account_name_l);
+  printf("Account Holder Name : %s %s\n", users[n].account_name_f, users[n].account_name_l);
   printf("Account Number : %d\n", users[n].account_number);
   printf("Balance : %d\n", users[n].account_balance);
   printf("---------------------------------\n");
   sleep(2);
 }
 
-int login(uac users[30],int n)
+void saveUsers(uac users[30], int n)
+{
+  FILE *fp = fopen("Bank_Info.txt", "w");
+
+  for (int i = 0; i < n; i++)
+  {
+    encrypter(users, i);
+
+    fprintf(fp, "%d\n", users[i].id);
+    fprintf(fp, "First Name: %s\n", users[i].account_name_f);
+    fprintf(fp, "Last Name: %s\n", users[i].account_name_l);
+    fprintf(fp, "Password: %s\n", users[i].password);
+    fprintf(fp, "Account Number: %d\n", users[i].account_number);
+    fprintf(fp, "Account Balance: %d\n", users[i].account_balance);
+    fprintf(fp, "Recent Transaction: %d\n", users[i].trans);
+    fprintf(fp, "---------------------------------\n\n");
+
+    decrypter(users, i);
+  }
+
+  fclose(fp);
+}
+
+int login(uac users[30], int n)
 {
   while (1)
   {
-    int y=-1;
-    int lac;
+    int account_no;
+    char password[30];
+
     printf("Enter your account number : ");
-    scanf("%d",&lac);
-    int f=0;
-    for(int i=0;i<n;i++)
+    scanf("%d", &account_no);
+
+    int found = 0;
+
+    for (int i = 0; i < n; i++)
     {
-      if(lac==users[i].account_number)
+      if (account_no == users[i].account_number)
       {
-        char lp[30];
-        printf("Enter the Password : ");
-        inputPassword(lp);
-        if (strcmp(lp,users[i].password)==0)
+        found = 1;
+
+        printf("Enter the password : ");
+        inputPassword(password);
+
+        if (strcmp(password, users[i].password) == 0)
         {
-          f=2;
-          printf("You are logged in !!\n");
-          y=i;
-          break;
+          printf("Login Successful!\n");
+          printf("Welcome %s!\n", users[i].account_name_f);
+          sleep(2);
+          return i; 
         }
         else
         {
-          f=1;
-          printf("Wrong password!!!!\n");
-          sleep(1);
+          printf("Incorrect Password!\n");
           break;
         }
       }
-      else
-      {
-        f=1;
-      }
     }
-    if (f==1)
+
+    if (!found)
     {
-      int x ;
-      printf("Wrong account number or password!!!");
-      printf("Try again ?\n");
-      printf("Enter 1 to TRY AGAIN and 2 to go back to MAIN MENU : ");
-      scanf("%d",&x);
-      if (x==1)
-      {
-        sleep(1);
-        continue;
-      }
-      else if (x==2)
-      {
-        return -1;
-      }
-      else
-      {
-        printf("Invalid Input !!! going back to MAIN MENU\n");
-        return -1;
-      }
+      printf("Account number not found!!!!\n");
+      sleep(1);
+    }
+
+    int choice;
+
+    printf("\n1) Try Again\n");
+    printf("2) Main Menu\n");
+    printf("Enter your choice : ");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+      continue;
     }
     else
     {
-      return y;
+      return -1;
     }
   }
 }
 
-void deposit(uac users[30],int n)
+void deposit(uac users[30], int n)
 {
-  int z=login(users,n);
-  if (z==-1)
+  int z = login(users, n);
+  if (z == -1)
   {
     sleep(2);
     return;
@@ -191,19 +208,24 @@ void deposit(uac users[30],int n)
   {
     int inc;
     printf("Enter the amount of money to deposit : ");
-    scanf("%d",&inc);
-    users[z].account_balance=inc+users[z].account_balance;
+    scanf("%d", &inc);
+    users[z].account_balance = inc + users[z].account_balance;
     sleep(1);
-    users[z].trans=inc;
-    printf("Your current balance : %d\n",users[z].account_balance);
+    users[z].trans = inc;
+    printf("Your current balance : %d\n", users[z].account_balance);
+    saveUsers(users, n);
     sleep(2);
   }
 }
 
-void withdraw(uac users[30],int n)
+void withdraw(uac users[30], int n)
 {
-  int z=login(users,n);
-  if (users[z].account_balance==0)
+  int z = login(users, n);
+  if (z == -1)
+  {
+    return;
+  }
+  if (users[z].account_balance == 0)
   {
     printf("Not sufficient balance!!!!");
     sleep(2);
@@ -211,7 +233,7 @@ void withdraw(uac users[30],int n)
   }
   else
   {
-    if (z==-1)
+    if (z == -1)
     {
       sleep(2);
       return;
@@ -219,22 +241,28 @@ void withdraw(uac users[30],int n)
     else
     {
       int dec;
-      printf("Enter the amount of money to deposit : ");
-      scanf("%d",&dec);
-      users[z].account_balance=users[z].account_balance-dec;
+      printf("Enter the amount of money to withdraw : ");
+      scanf("%d", &dec);
+      if (dec > users[z].account_balance)
+      {
+        printf("Not sufficient money !!!!\n");
+        sleep(2);
+        return;
+      }
+      users[z].account_balance = users[z].account_balance - dec;
       sleep(1);
-      users[z].trans=dec*-1;
-      printf("Your current balance : %d\n",users[z].account_balance);
+      users[z].trans = dec * -1;
+      printf("Your current balance : %d\n", users[z].account_balance);
+      saveUsers(users, n);
       sleep(2);
     }
   }
-  
 }
 
-void check(uac users[30],int n)
+void check(uac users[30], int n)
 {
-  int z=login(users,n);
-  if (z==-1)
+  int z = login(users, n);
+  if (z == -1)
   {
     sleep(2);
     return;
@@ -244,18 +272,20 @@ void check(uac users[30],int n)
     printf("---------------------------------\n");
     printf("           YOUR DETAILS          \n");
     printf("---------------------------------\n");
-    printf("Account Holder Name : %s %s\n", users[n].account_name_f,users[n].account_name_l);
-    printf("Account Number : %d\n", users[n].account_number);
-    printf("Balance: %d\n", users[n].account_balance);
-    printf("Recent Transaction: %d\n", users[n].trans);
+    printf("Account Holder Name : %s %s\n", users[z].account_name_f, users[z].account_name_l);
+    printf("Account Number : %d\n", users[z].account_number);
+    printf("Balance: %d\n", users[z].account_balance);
+    printf("Recent Transaction: %d\n", users[z].trans);
     printf("---------------------------------\n");
+    
     printf("Enter any key to exit : ");
     char c;
-    scanf("%c",&c);
+    getchar();
+    scanf("%c", &c);
   }
 }
 
-// MAIN 
+// MAIN
 
 int main()
 {
@@ -263,15 +293,15 @@ int main()
   uac users[30];
   int n;
   FILE *ptr1;
-  ptr1=fopen("userno.txt","r");
-  fscanf(ptr1,"%d",&n);
+  ptr1 = fopen("userno.txt", "r");
+  fscanf(ptr1, "%d", &n);
   fclose(ptr1);
   FILE *ptr3;
-  ptr3=fopen("Bank_Info.txt","r");
-  for (int i = 0 ; i < n ; i++)
+  ptr3 = fopen("Bank_Info.txt", "r");
+  for (int i = 0; i < n; i++)
   {
-   fscanf(ptr3,"%d\nFirst Name: %s\nLast Name: %s\nPassword: %s\nAccount Number: %d\nAccount Balance: %d\nRecent Transaction: %d\n---------------------------------\n\n",&users[i].id,users[i].account_name_f,users[i].account_name_l,users[i].password,&users[i].account_number,&users[i].account_balance,&users[i].trans);
-   decrypter(users,i);
+    fscanf(ptr3, "%d\nFirst Name: %s\nLast Name: %s\nPassword: %s\nAccount Number: %d\nAccount Balance: %d\nRecent Transaction: %d\n---------------------------------\n\n", &users[i].id, users[i].account_name_f, users[i].account_name_l, users[i].password, &users[i].account_number, &users[i].account_balance, &users[i].trans);
+    decrypter(users, i);
   }
   fclose(ptr3);
   int ch;
@@ -301,18 +331,18 @@ int main()
         create_account(users, n);
         n++;
         FILE *ptr2;
-        ptr2=fopen("userno.txt","w");
-        fprintf(ptr2,"%d",n);
+        ptr2 = fopen("userno.txt", "w");
+        fprintf(ptr2, "%d", n);
         fclose(ptr2);
       }
     }
     else if (ch == 2)
     {
-      withdraw(users,n);
+      withdraw(users, n);
     }
     else if (ch == 3)
     {
-      deposit(users,n);
+      deposit(users, n);
     }
     else if (ch == 5)
     {
@@ -320,9 +350,9 @@ int main()
       sleep(1);
       break;
     }
-    else if (ch==4)
+    else if (ch == 4)
     {
-      check(users,n);
+      check(users, n);
     }
     else
     {
