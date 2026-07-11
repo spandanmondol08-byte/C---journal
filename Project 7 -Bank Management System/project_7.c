14f9fb5ba2acd8697a088f8940ec9a0729513efe
Spandan <spandanmondol08@gmail.com>
2026-07-11 21:49:59 +0530

    Add : Project 7 v5

--- Project 7 -Bank Management System/project_7.c
+++ Project 7 -Bank Management System/project_7.c
@@ -13,6 +13,7 @@ typedef struct user_accounts
   char password[30];
   int account_number;
   int account_balance;
+  int trans;
 } uac;
 
 void encrypter(uac users[30],int n)
@@ -80,6 +81,7 @@ void create_account(uac users[30], int n)
     }
   }
   users[n].account_balance = 0;
+  users[n].trans = 0;
   FILE *ptr;
   ptr = fopen("Bank_Info.txt", "a");
   users[n].id=n+1;
@@ -99,6 +101,7 @@ void create_account(uac users[30], int n)
   fprintf(ptr, "Password: %s\n", users[n].password);
   fprintf(ptr, "Account Number: %d\n", users[n].account_number);
   fprintf(ptr, "Account Balance: %d\n", users[n].account_balance);
+  fprintf(ptr, "Recent Transaction: %d\n", users[n].trans);
   fprintf(ptr,"---------------------------------\n");
   fprintf(ptr, "\n");
   fclose(ptr);
@@ -106,7 +109,7 @@ void create_account(uac users[30], int n)
   printf("---------------------------------\n");
   printf("Account Holder Name : %s %s\n", users[n].account_name_f,users[n].account_name_l);
   printf("Account Number : %d\n", users[n].account_number);
-  printf("Balance: %d\n", users[n].account_balance);
+  printf("Balance : %d\n", users[n].account_balance);
   printf("---------------------------------\n");
   sleep(2);
 }
@@ -115,25 +118,30 @@ int login(uac users[30],int n)
 {
   while (1)
   {
+    int y=-1;
     int lac;
     printf("Enter your account number : ");
     scanf("%d",&lac);
     int f=0;
     for(int i=0;i<n;i++)
     {
-      if(lac=users[i].account_number)
+      if(lac==users[i].account_number)
       {
         char lp[30];
         printf("Enter the Password : ");
         inputPassword(lp);
-        if (lp=users[i].password)
+        if (strcmp(lp,users[i].password)==0)
         {
+          f=2;
           printf("You are logged in !!\n");
+          y=i;
           break;
         }
         else
         {
           f=1;
+          printf("Wrong password!!!!\n");
+          sleep(1);
           break;
         }
       }
@@ -145,8 +153,9 @@ int login(uac users[30],int n)
     if (f==1)
     {
       int x ;
+      printf("Wrong account number or password!!!");
       printf("Try again ?\n");
-      printf("Enter 1 to TRY AGAIN and 2 to go back to MAIN MENU :");
+      printf("Enter 1 to TRY AGAIN and 2 to go back to MAIN MENU : ");
       scanf("%d",&x);
       if (x==1)
       {
@@ -155,18 +164,94 @@ int login(uac users[30],int n)
       }
       else if (x==2)
       {
-        return m;
+        return -1;
       }
       else
       {
         printf("Invalid Input !!! going back to MAIN MENU\n");
-        return m;
+        return -1;
       }
     }
     else
     {
-      return con;
+      return y;
     }
+  }
+}
+
+void deposit(uac users[30],int n)
+{
+  int z=login(users,n);
+  if (z==-1)
+  {
+    sleep(2);
+    return;
+  }
+  else
+  {
+    int inc;
+    printf("Enter the amount of money to deposit : ");
+    scanf("%d",&inc);
+    users[z].account_balance=inc+users[z].account_balance;
+    sleep(1);
+    users[z].trans=inc;
+    printf("Your current balance : %d\n",users[z].account_balance);
+    sleep(2);
+  }
+}
+
+void withdraw(uac users[30],int n)
+{
+  int z=login(users,n);
+  if (users[z].account_balance==0)
+  {
+    printf("Not sufficient balance!!!!");
+    sleep(2);
+    return;
+  }
+  else
+  {
+    if (z==-1)
+    {
+      sleep(2);
+      return;
+    }
+    else
+    {
+      int dec;
+      printf("Enter the amount of money to deposit : ");
+      scanf("%d",&dec);
+      users[z].account_balance=users[z].account_balance-dec;
+      sleep(1);
+      users[z].trans=dec*-1;
+      printf("Your current balance : %d\n",users[z].account_balance);
+      sleep(2);
+    }
+  }
+  
+}
+
+void check(uac users[30],int n)
+{
+  int z=login(users,n);
+  if (z==-1)
+  {
+    sleep(2);
+    return;
+  }
+  else
+  {
+    printf("---------------------------------\n");
+    printf("           YOUR DETAILS          \n");
+    printf("---------------------------------\n");
+    printf("Account Holder Name : %s %s\n", users[n].account_name_f,users[n].account_name_l);
+    printf("Account Number : %d\n", users[n].account_number);
+    printf("Balance: %d\n", users[n].account_balance);
+    printf("Recent Transaction: %d\n", users[n].trans);
+    printf("---------------------------------\n");
+    printf("Enter any key to exit : ");
+    char c;
+    scanf("%c",&c);
   }
 }
 
@@ -185,7 +270,7 @@ int main()
   ptr3=fopen("Bank_Info.txt","r");
   for (int i = 0 ; i < n ; i++)
   {
-   fscanf(ptr3,"%d\nFirst Name: %s\nLast Name: %s\nPassword: %s\nAccount Number: %d\nAccount Balance: %d\n---------------------------------\n\n",&users[i].id,users[i].account_name_f,users[i].account_name_l,users[i].password,&users[i].account_number,&users[i].account_balance);
+   fscanf(ptr3,"%d\nFirst Name: %s\nLast Name: %s\nPassword: %s\nAccount Number: %d\nAccount Balance: %d\nRecent Transaction: %d\n---------------------------------\n\n",&users[i].id,users[i].account_name_f,users[i].account_name_l,users[i].password,&users[i].account_number,&users[i].account_balance,&users[i].trans);
    decrypter(users,i);
   }
   fclose(ptr3);
@@ -198,7 +283,8 @@ int main()
     printf("1) Create Account\n");
     printf("2) Withdraw Money\n");
     printf("3) Deposit Money\n");
-    printf("4) EXIT!!!\n");
+    printf("4) Check Balance\n");
+    printf("5) EXIT!!!\n");
     printf("---------------------------------\n");
     printf("Enter your choice : ");
     scanf("%d", &ch);
@@ -222,15 +308,21 @@ int main()
     }
     else if (ch == 2)
     {
+      withdraw(users,n);
     }
     else if (ch == 3)
     {
+      deposit(users,n);
     }
-    else if (ch == 4)
+    else if (ch == 5)
     {
       printf("EXITING !!!\n");
       sleep(1);
       break;
+    }
+    else if (ch==4)
+    {
+      check(users,n);
     }
     else
     {
@@ -238,6 +330,5 @@ int main()
       sleep(1);
     }
   }
-
   return 0;
 }
