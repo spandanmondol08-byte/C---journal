be1787df1aaf7bd0f4a2a9c4d865947b4ea95a3f
Spandan <spandanmondol08@gmail.com>
2026-07-12 22:14:16 +0530

    Add : Project 8

--- Project 8 -Sudoku Solver/project_8.c
+++ Project 8 -Sudoku Solver/project_8.c
@@ -53,10 +53,64 @@ void print_puzzle(int puzzle[9][9])
   printf("+---------+---------+---------+\n");
 }
 
+int valid_move(int puzzle[9][9],int row,int column,int value)
+{
+  // Row check
+  for (int i = 0 ; i<9;i++)
+  {
+    if (puzzle[row][i] == value)
+    {
+      return 0;
+    }
+  }
+  // Column check
+  for (int i = 0 ; i<9;i++)
+  {
+    if (puzzle[i][column] == value)
+    {
+      return 0;
+    }
+  }
+  // Box check 
+  int r = row - row%3;
+  int c = column - column%3;
+  for (int i =0 ; i <3 ; i++)
+  {
+    for (int j = 0 ; j < 3 ; j++)
+    {
+      if (puzzle[r+i][c+j]==value)
+      {
+        return 0;
+      }
+      
+    }
+  }
+ return 1;
+}
+
 // MAIN
 
 int main()
 {
   print_puzzle(puzzle);
+  while (1)
+  {
+    for ( int i = 0 ; i < 9 ; i++)
+    {
+      for ( int j =0 ; j < 9 ; j++)
+      {
+        int val =0;
+        if (valid_move(puzzle,i,j,val)==0)
+        {
+          val++;
+        }
+        else
+        {
+          puzzle[i][j]==val;
+        }
+      }
+    }
+  }
+  
   return 0;
 }
