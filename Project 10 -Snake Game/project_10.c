493991adc17e4468ee44ee26354d39352e9d1608
Spandan <spandanmondol08@gmail.com>
2026-07-15 15:41:29 +0530

    ADD : Project 10 tail

--- Project 10 -Snake Game/project_10.c
+++ Project 10 -Snake Game/project_10.c
@@ -7,7 +7,7 @@
 int tail_x[100];
 int tail_y[100];
 int tail_length = 0;
-int height=20 , width =60;
+int height = 20, width = 60;
 int food_x;
 int food_y;
 int snake_x;
@@ -17,11 +17,27 @@ int speed = 200;
 void entry();
 void draw();
 void setup();
-void game_loop();
+int isTail(int, int);
+void updateTail();
 
+void gotoxy(int x, int y)
+{
+  COORD pos = {x, y};
+  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+void hideCursor()
+{
+  HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_CURSOR_INFO info;
+
+  info.dwSize = 100;
+  info.bVisible = FALSE;
+
+  SetConsoleCursorInfo(h, &info);
+}
 void entry()
 {
-  char ch='x';
+  char ch = 'x';
   setup();
   draw();
   while (1)
@@ -30,20 +46,24 @@ void entry()
     {
       ch = getch();
     }
-    if (ch=='w')
+    if (ch == 'w')
     {
+      updateTail();
       snake_y--;
     }
-    else if (ch=='s')
+    else if (ch == 's')
     {
+      updateTail();
       snake_y++;
     }
-    else if (ch=='a')
+    else if (ch == 'a')
     {
+      updateTail();
       snake_x--;
     }
-    else if (ch=='d')
+    else if (ch == 'd')
     {
+      updateTail();
       snake_x++;
     }
     if (snake_y < 0)
@@ -54,7 +74,7 @@ void entry()
     if (snake_y >= height)
     {
       snake_y = 0;
-    } 
+    }
 
     if (snake_x < 0)
     {
@@ -66,72 +86,103 @@ void entry()
       snake_x = 0;
     }
 
-    if(snake_x==food_x && snake_y==food_y)
+    if (snake_x == food_x && snake_y == food_y)
     {
+      tail_length++;
       score += 10;
       speed -= 10;
       if (speed < 50)
       {
         speed = 50;
       }
-      food_x=rand()%width;
-      food_y=rand()%height;
+      food_x = rand() % width;
+      food_y = rand() % height;
     }
-    draw();
+
     Sleep(speed);
+    draw();
   }
 }
 
 void draw()
 {
-  system("cls");
-  printf("\t\t\t\tSNAKE GAME\n\n");
-  for (int i =0 ; i < width+2 ; i++)
+  gotoxy(0, 0);
+  // printf("\t\t\t\tSNAKE GAME\n\n");
+  for (int i = 0; i < width + 2; i++)
   {
     printf("#");
   }
   printf("\n");
 
-  for (int i = 0 ; i < height ; i++)
+  for (int i = 0; i < height; i++)
   {
     printf("#");
-    for (int j = 0; j < width ; j++)
+    for (int j = 0; j < width; j++)
     {
-      
-      if (i==snake_y && j==snake_x)
+
+      if (i == snake_y && j == snake_x)
       {
         printf("O");
       }
-      else if (i==food_y && j==food_x)
+      else if (i == food_y && j == food_x)
       {
         printf("F");
+      }
+      else if (isTail(j,i))
+      {
+        printf("o");
       }
       else
       {
         printf(" ");
       }
-      
     }
     printf("#\n");
   }
 
-  for (int i =0 ; i < width+2 ; i++)
+  for (int i = 0; i < width + 2; i++)
   {
     printf("#");
   }
-  printf("\n\nScore : %d\n\n",score);
-
+  printf("\n\nScore : %d\n\n", score);
 }
 
 void setup()
 {
-  snake_y=height/2;
-  snake_x=width/2;
-  food_x=rand()%width;
-  food_y=rand()%height;
+  snake_y = height / 2;
+  snake_x = width / 2;
+  food_x = rand() % width;
+  food_y = rand() % height;
 }
 
-int main() {
+void updateTail()
+{
+    for (int i = tail_length - 1; i > 0; i--)
+    {
+        tail_x[i] = tail_x[i - 1];
+        tail_y[i] = tail_y[i - 1];
+    }
+
+    tail_x[0] = snake_x;
+    tail_y[0] = snake_y;
+}
+
+int isTail(int x, int y)
+{
+  for (int i = 0; i < tail_length; i++)
+  {
+    if (tail_x[i] == x && tail_y[i] == y)
+    {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+int main()
+{
+  hideCursor();
   srand(time(NULL));
   entry();
   return 0;
