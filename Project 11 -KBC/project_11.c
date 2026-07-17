fe64b3b89277e29db3817760b8d91f639e969c07
Spandan <spandanmondol08@gmail.com>
2026-07-17 21:36:23 +0530

    ADD : Project 11 new func

--- Project 11 -KBC/project_11.c
+++ Project 11 -KBC/project_11.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+// STRUCTURE DEFINATION
+
 typedef struct KBC
 {
   char question[300];
@@ -10,10 +12,15 @@ typedef struct KBC
   int prize;
 }q_struct;
 
-int question_nos=20;
 q_struct questions[20];
 
-void read(q_struct questions[])
+// FUNCTIONS
+
+
+void read(q_struct questions[],int);
+void qno(int);
+
+void read(q_struct questions[],int question_nos)
 {
   FILE * ptr;
   ptr = fopen("questions.txt","r");
@@ -57,20 +64,45 @@ void read(q_struct questions[])
     fscanf(ptr,"Correct: %c\n",&questions[i].correct_op);
     fscanf(ptr,"Time: %d\n",&questions[i].timeout);
     fscanf(ptr,"Prize: %d\n",&questions[i].prize);
-    printf("%s",questions[i].question);
-    printf("%s",questions[i].options[0]);
-    printf("%s",questions[i].options[1]);
-    printf("%s",questions[i].options[2]);
-    printf("%s",questions[i].options[3]);
-    printf("%c",questions[i].correct_op);
-    printf("%d",questions[i].timeout);
-    printf("%d",questions[i].prize);
   }
   fclose(ptr);
 }
 
+void qno(int question_nos)
+{
+  int c=0;
+  FILE * ptr1;
+  ptr1=fopen("questions.txt","r");
+  if (ptr1 == NULL)
+  {
+    printf("Cannot open questions.txt\n");
+    return;
+  }
+  while (1)
+  {
+    char ch=fgetc(ptr1);
+    if (ch==EOF)
+    {
+      fclose(ptr1);
+      break;
+    }
+    else
+    {
+      if (ch=='\n')
+      {
+        c++;
+      }
+    }
+  }
+  c++;
+  question_nos=c/8;
+}
+
+// MAIN
 
 int main() {
-  read(questions);
+  int question_nos ;
+  qno(question_nos);
+  read(questions,question_nos);
   return 0;
 }
